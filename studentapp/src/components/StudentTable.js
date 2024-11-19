3ab6179c1d4eb7649812b58f31e723d42c9eeb34
prathibhani <139643234+prathibhani@users.noreply.github.com>
2024-11-19 16:16:20 +0000

    Student table layout out and styles are done

--- studentapp/src/components/StudentTable.js
+++ studentapp/src/components/StudentTable.js
@@ -1,7 +1,20 @@
-export default function StudentTable(prosps){
+import '../assests/css/compo.css';
+export default function StudentTable(props){
     return(
-        <div>
+        <div className="outerDiv">
+            <div className="leftDiv">
+               <table>
+                <thead> 
+                    <tr>
+                        <td>First Name</td>
+                        <td>Last Name</td>
+                        <td>Country</td>
+                        <td>Course</td>
 
+                    </tr>
+                </thead>
+               </table>
+             </div>
         </div>
     );
 }
